8927cc2da4f6710e68075a73233780e2ed449a4e
alexander <boiko.s2005@gmail.com>
2025-10-21 23:30:38 +0300

    init

---
+++ src/main/java/com/rendaxx/labs/InformationSystemsLabsApplication.java
@@ -0,0 +1,13 @@
+package com.rendaxx.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemsLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationSystemsLabsApplication.class, args);
+    }
+
+}
