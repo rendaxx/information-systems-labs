a24a07cec8361f4039cbfd9108b9e4b88c3302c1
alexander <boiko.s2005@gmail.com>
2025-12-07 18:48:08 +0300

    testcontainer non-static init

--- apps/backend/src/test/java/com/rendaxx/labs/controller/support/IntegrationTest.java
+++ apps/backend/src/test/java/com/rendaxx/labs/controller/support/IntegrationTest.java
@@ -8,25 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(initializers = PostgresInitializer.class)
 public abstract class IntegrationTest {
-
-    @DynamicPropertySource
-    static void configureDatasourceProperties(DynamicPropertyRegistry registry) {
-        PostgreSQLContainer<?> postgres = PostgresTestContainer.get();
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.flyway.url", postgres::getJdbcUrl);
-        registry.add("spring.flyway.user", postgres::getUsername);
-        registry.add("spring.flyway.password", postgres::getPassword);
-    }
 
     @Autowired
     protected MockMvc mockMvc;
